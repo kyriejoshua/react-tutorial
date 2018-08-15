b01aa32e5bb7e19a03135121bbd654e98cf4f95d
樟子木 <zhangzimu@wacai.com>
2018-08-15 20:12:25 +0800

    chore: 添加配置修复无法访问的问题

--- react-router-deep/routes/index.js
+++ react-router-deep/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Router, Route } from 'react-router'
 import Home from './../app/home'
 
-// TODO home 页面暂时无法跳转
+const prefix = process.env.prefix
+// TODO prefix 前缀用法需要特殊处理
 const AppRouter = ({ history }) => (
   <Router history={history}>
     <div id="content">
-      <Route exact={true} path='/' render={() => (<h1>Index</h1>)} />
+      <Route exact path='/' render={() => (<h1>Index</h1>)} />
       <Route path='/home' component={Home} />
+      {/* <Route path={`${prefix}/home`} component={Home} /> */}
     </div>
   </Router>
 )
--- react-router-deep/webpack.config.js
+++ react-router-deep/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const routerPrefix = '/react-router'
 
 module.exports = {
   entry: './index.js',
@@ -30,7 +32,18 @@ module.exports = {
       }
     ]
   },
+  // https://webpack.docschina.org/configuration/dev-server/#devserver-historyapifallback
+  // /home 页面无法访问的解决办法，这是在 webpack-dev-server 层面需要支持的
+  // 当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 index.html。通过传入以下启用：
+  devServer: {
+    historyApiFallback: true,
+  },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        prefix: JSON.stringify(routerPrefix)
+      }
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html'
